ac6c1832a0e52015387d580b40afec7bd0d0c5e8
tomascallenius <tomascallenius96@gmail.com>
2023-08-16 22:41:22 -0300

    asd

--- src/contextClient/config/axiosClient.jsx
+++ src/contextClient/config/axiosClient.jsx
@@ -1,8 +1,11 @@
+require("dotenv").config();
+const { RAILWAY_BACKEND_URL, VITE_BACKEND_URL } = process.env;
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: `${import.meta.env.RAILWAY_BACKEND_URL || import.meta.env.VITE_BACKEND_URL }`,
-  
+  baseURL: `${
+    RAILWAY_BACKEND_URL || VITE_BACKEND_URL
+  }`,
 });
 // console.log(import.meta.env.);
 
