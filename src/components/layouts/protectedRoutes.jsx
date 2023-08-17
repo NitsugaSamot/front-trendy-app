6915ddbc0cdcea208334de2f891e73d7dbbd7fd7
tomascallenius <tomascallenius96@gmail.com>
2023-08-16 21:08:05 -0300

    asdqwe

---
+++ src/components/layouts/protectedRoutes.jsx
@@ -0,0 +1,12 @@
+import { Outlet, Navigate } from "react-router-dom";
+import useAuth from "../../contextClient/hooks/useAuth";
+
+const ProtectedRoutes = () => {
+  const { auth, cargando } = useAuth();
+
+  if (cargando) return "Cargando...";
+
+  return <>{auth.id ? <Outlet /> : <Navigate to="/login" />}</>;
+};
+
+export default ProtectedRoutes;
