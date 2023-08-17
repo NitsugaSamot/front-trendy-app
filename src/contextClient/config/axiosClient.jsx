e81c094c21676663724d662a973a977aa2d4ea4f
tomascallenius <tomascallenius96@gmail.com>
2023-08-17 00:48:30 -0300

    a ver ahora5

--- src/contextClient/config/axiosClient.jsx
+++ src/contextClient/config/axiosClient.jsx
@@ -1,8 +1,9 @@
 import axios from "axios";
+const RAILWAY_BACKEND_URL = process.env.RAILWAY_BACKEND_URL;
+const VITE_BACKEND_URL = process.env.VITE_BACKEND_URL;
 
 const axiosClient = axios.create({
-  baseURL: `${import.meta.env.RAILWAY_BACKEND_URL || import.meta.env.VITE_BACKEND_URL }`,
-  
+  baseURL: `${RAILWAY_BACKEND_URL || VITE_BACKEND_URL}`,
 });
 // console.log(import.meta.env.);
 
