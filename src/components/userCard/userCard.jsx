115f32e9e13ce3b496532469dea422503f6296bd
F.Fernandez <hellsingsd@gmail.com>
2023-08-23 03:16:12 -0300

    jojojo

--- src/components/userCard/userCard.jsx
+++ src/components/userCard/userCard.jsx
@@ -4,10 +4,10 @@ const UserCard = ({ user }) => {
     return (
         <div className="UserConfig">
             <h1>{user.id}</h1>
-            <h1>{user.name}</h1>
+           {/*  <h1>{user.name}</h1>
             <h1>{user.type}</h1>
             <h1>{user.confirmated ? "Confirmed" : "Not Confirmed"}</h1>
-            <h1>{user.isDeleted ? "Deleted" : "Active"}</h1>
+            <h1>{user.isDeleted ? "Deleted" : "Active"}</h1> */}
         </div>
     )
 }
