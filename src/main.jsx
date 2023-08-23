3a5931247c44afd744b4ef6a7be2fa8cba6e010c
F.Fernandez <hellsingsd@gmail.com>
2023-08-23 02:28:05 -0300

    hooaaaa

--- src/main.jsx
+++ src/main.jsx
@@ -10,9 +10,7 @@ import store from "./redux/store.js";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
-      <React.StrictMode>
         <App />
-      </React.StrictMode>
     </BrowserRouter>
   </Provider>
 );
