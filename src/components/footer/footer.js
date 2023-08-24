0ce7bab3cce580a21ca01b3e82d920d665bb82cb
Lokerrr <juan53@live.com.ar>
2023-08-24 10:49:58 -0300

    re jugados con el facha y rober y facu

---
+++ src/components/footer/footer.js
@@ -0,0 +1,45 @@
+import { FaStore, FaPhone } from "react-icons/fa"
+import {MdEmail} from "react-icons/md"
+import "./footer.css"; 
+import image from "../../assets/trendy-spot-logo.png"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="top-bar">
+        <img id="logo" src={image} alt="Logo de la empresa" />
+        <nav className="menu">
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Us</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+      <div className="main-section">
+        <div className="about-us">
+          <h3>About us</h3>
+          <p>Somos una empresa especializada en la venta de productos deportivos de alta calidad. Nuestro objetivo es brindar soluciones excepcionales para mejorar tu rendimiento y disfrutar al máximo de tu pasión por el deporte.</p>
+        </div>
+        <div className="contact-us">
+          <h3>Contáctanos</h3>
+          <FaStore /> <span>Calle Roca 123, Ciudad Corrientes, País Argentina</span><br />
+          <FaPhone/>  <span>+00 000 000 000</span><br />
+          <MdEmail/> <span>info@empresa.com</span><br />
+        </div>
+      </div>
+      <div className="bottom-bar">
+        <i className="fab fa-facebook-f"></i>
+        <i className="fab fa-twitter"></i>
+        <i className="fab fa-instagram"></i>
+        <i className="fab fa-linkedin-in"></i>
+        <a id="back-to-top" href="#"><i className="fas fa-angle-up"></i></a>
+      </div>
+      <div className="copy-right">
+        <span> &copy;2023 Trendy Spot, todos los derechos reservados.</span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -1,21 +1,20 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
-import CardsContainer from '../../components/cardsContainer/cardsContainer'
+import CardsContainer from "../../components/cardsContainer/cardsContainer";
 import Filter from "../../components/filter/filter";
-import Paginate from '../../components/paginate/paginate';
-import banner from '../../assets/Home.png'
+import Paginate from "../../components/paginate/paginate";
+import banner from "../../assets/Home.png";
 import "./home.css";
-import { NavLink } from 'react-router-dom';
+import Footer from "../../components/footer/footer";
 
 const Home = () => {
   const allClothes1 = useSelector((state) => state.allClothes1);
-console.log(allClothes1);
+  console.log(allClothes1);
   const [currentPage, setCurrentPage] = useState(1);
   const productsPerPage = 12;
 
   useEffect(() => {
-    setCurrentPage(1)
-
+    setCurrentPage(1);
   }, [allClothes1]);
 
   const handlePageChange = (page) => {
@@ -37,13 +36,18 @@ console.log(allClothes1);
   }
 
   return (
-  <div>
-    <img src={banner} className="background-image" />
-    <div className="container py-3">
-      <Filter onPageChange={(page) => setCurrentPage(page)}/>
-      <CardsContainer currentProduct={currentProduct} />
-      <Paginate currentPage={currentPage} totalPages={totalPages} handlePageChange={handlePageChange}/>
-    </div>
+    <div>
+      <img src={banner} className="background-image" />
+      <div className="container py-3">
+        <Filter onPageChange={(page) => setCurrentPage(page)} />
+        <CardsContainer currentProduct={currentProduct} />
+        <Paginate
+          currentPage={currentPage}
+          totalPages={totalPages}
+          handlePageChange={handlePageChange}
+        />
+      </div>
+      <Footer />
     </div>
   );
 };
