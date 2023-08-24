cacf1a8f1cde371e5939e1c1716ddccca4e41429
Tomas <69489194+NitsugaSamot@users.noreply.github.com>
2023-08-24 12:49:44 -0300

    Update confirmAccount.jsx

--- src/components/login/confirmAccount.jsx
+++ src/components/login/confirmAccount.jsx
@@ -43,7 +43,7 @@ const ConfirmAccount = () => {
       <div>Confirm Account</div>
 
       <div className="mt-20 md:mt-10 shadow-lg px-5 py-10 rounded-xl bg-white">
-        {msg && <Alert alerta={alert} />}
+        {msg && <Alert alert={alert} />}
 
         {accountConfirmated && (
           <Link
