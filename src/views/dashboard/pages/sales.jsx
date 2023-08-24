e61e59f5ed7d9e72adb2d7bc149f20911bbc4cbc
Lokerrr <juan53@live.com.ar>
2023-08-24 12:49:25 -0300

    123

--- src/views/dashboard/pages/sales.jsx
+++ src/views/dashboard/pages/sales.jsx
@@ -1,7 +1,37 @@
+import axios from "axios";
+import { useState } from "react";
+
 const Sales = () => {
+
+    const [users, setUsers] = useState()
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`https://back-trendy-app.up.railway.app/users/all-purchases`
+                );
+                const { data } = response;
+                setUsers(data);
+            } catch (error) {
+                window.alert("Error al obtener los datos del personaje");
+            }
+        };
+        fetchData();
+    }, []);
+
     return (
-      <div>ACA ESTAN TODOS TUS VENTASSS</div>
+        <div>
+        {
+            users.map((user, index) => (
+                <div key={index}>
+                  <h1>{user.userName}</h1> // Usar user.userId para mostrar el ID de usuario
+                  <h1>{purchaseDetails.title}</h1>
+                  <h1>{purchaseDetails.unit_price}</h1>
+                </div>
+            ))
+        }
+    </div>
     )
-  }
-   
-  export default Sales;
+
+    }
+export default Sales
