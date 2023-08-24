ca8281e60a69b24038fcd61a74dad474ea787a96
Lokerrr <juan53@live.com.ar>
2023-08-24 03:41:18 -0300

    salvando papas 2.8

--- src/views/dashboard/pages/inicio.jsx
+++ src/views/dashboard/pages/inicio.jsx
@@ -1,6 +1,10 @@
+import logo from "../../../assets/trendy-spot-logo.png"
+import Image from 'react-bootstrap/Image';
+
 const Inicio = () => {
     return (
       <div>
+        <Image src={logo} fluid/>
         <h1>SOY EL INICIO BUENASSS</h1>
       </div>
     )
